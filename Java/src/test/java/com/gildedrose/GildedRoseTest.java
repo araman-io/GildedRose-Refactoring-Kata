a2785aab607bb45cb5b8a3d6ceb74f30a4ad0cf6
anand.raman@kingfisher.com <anand.raman@kingfisher.com>
2024-10-05 20:43:36 +0100

    ensured that quality and sellin dates decrement with updateQuality() call

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,10 +8,20 @@ class GildedRoseTest {
 
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
     }
 
+    @Test
+    void quality_degardes_every_day() {
+        Item item = new Item("foo", 5, 10);
+        GildedRose app = new GildedRose(new Item[]{item});
+        app.updateQuality();
+
+        assertEquals(4, app.items[0].sellIn, "sell in should have lowered by 1");
+        assertEquals(9, app.items[0].quality, "quality should have lowered by 1");
+    }
+    
 }
